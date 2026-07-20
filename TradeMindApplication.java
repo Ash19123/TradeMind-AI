598dbaeb511c24cd0847968e964bcc84e5dd9703
Ash19123 <aasritha.dakshinyam@gmail.com>
2026-07-20 20:22:09 +0530

    Create TradeMindApplication.java

---
+++ TradeMindApplication.java
@@ -0,0 +1,13 @@
+package com.trademind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMindApplication.class, args);
+    }
+
+}
